cd00d33c80ab2c88a4c6734648015e2f57c1bef0
Venkat Arun <venkatarun95@gmail.com>
2023-01-06 15:27:07 -0500

    Animation of ideal link

--- job_talk/anim/src/main.rs
+++ job_talk/anim/src/main.rs
@@ -88,9 +88,8 @@ impl<N: Element> Element for Transport<N> {
     }
 
     fn enqueue(&mut self, pkt: &Packet) {
-        let mut pkt = pkt.clone();
         self.pkts.push((
-            pkt,
+            *pkt,
             0,
             pkt.coord,
             self.next.borrow_mut().get_enqueue_coord(),
@@ -148,6 +147,32 @@ impl Element for Sink {
     }
 }
 
+/// Simply acks packets back
+struct Acker<N: Element> {
+    pub rcv_coord: Coord,
+    pub snd_coord: Coord,
+    pub next: Rc<RefCell<N>>,
+}
+
+impl<N: Element> Element for Acker<N> {
+    fn get_enqueue_coord(&self) -> Coord {
+        self.rcv_coord
+    }
+    fn enqueue(&mut self, pkt: &Packet) {
+        let mut pkt = pkt.clone();
+        pkt.size = ACK_PKT_WIDTH;
+        pkt.coord = self.snd_coord;
+        self.next.borrow_mut().enqueue(&pkt);
+    }
+    fn get_pkts(&self) -> Vec<Packet> {
+        Vec::new()
+    }
+    fn tick(&mut self) {}
+    fn draw<DB: DrawingBackend>(&self) -> Vec<DynElement<DB, (f64, f64)>> {
+        Vec::new()
+    }
+}
+
 struct Bottleneck<N: Element> {
     /// Coords of the left center of the buffer
     coord: Coord,
@@ -161,14 +186,23 @@ struct Bottleneck<N: Element> {
     /// Used to drop packets
     dropper: Transport<Sink>,
     next: Rc<RefCell<N>>,
+    /// +1 means left to right, -1 means right to left
+    dir: f64,
 }
 
 impl<N: Element> Bottleneck<N> {
-    fn new(coord: Coord, bufsize: u64, intersend_time: u64, next: Rc<RefCell<N>>) -> Self {
+    /// dir = true means left to right and false means right to left
+    fn new(
+        coord: Coord,
+        bufsize: u64,
+        intersend_time: u64,
+        next: Rc<RefCell<N>>,
+        dir: bool,
+    ) -> Self {
         let dropper = Transport::new(
             16,
             Rc::new(RefCell::new(Sink {
-                coord: coord.sub(Coord(0., 5.)),
+                coord: coord.sub(Coord(DATA_PKT_WIDTH, 5.)),
             })),
         );
 
@@ -180,6 +214,7 @@ impl<N: Element> Bottleneck<N> {
             time_since_last_deque: 0,
             dropper,
             next,
+            dir: if dir { 1.0 } else { -1.0 },
         }
     }
 }
@@ -193,14 +228,13 @@ impl<N: Element> Element for Bottleneck<N> {
     fn enqueue(&mut self, pkt: &Packet) {
         if self.pkts.len() < self.bufsize as usize {
             let mut pkt = pkt.clone();
-            let bufwidth = self.pkts.iter().map(|p| p.size).sum();
+            let bufwidth: f64 = self.pkts.iter().map(|p| p.size).sum();
             pkt.coord = self
                 .coord
-                .sub(Coord(bufwidth, 0.))
-                .add(Coord(self.bufsize as f64 * DATA_PKT_WIDTH, 0.))
-                .sub(Coord(pkt.size, 0.));
+                .sub(Coord(bufwidth * self.dir, 0.))
+                .add(Coord(self.bufsize as f64 * DATA_PKT_WIDTH * self.dir, 0.))
+                .sub(Coord(pkt.size * self.dir, 0.));
             self.pkts.push_back(pkt);
-            println!("{:?}", self.pkts);
         } else {
             self.dropper.enqueue(pkt);
         }
@@ -217,7 +251,7 @@ impl<N: Element> Element for Bottleneck<N> {
             let popped = self.pkts.pop_front().unwrap();
             self.next.borrow_mut().enqueue(&popped);
             for mut pkt in &mut self.pkts {
-                pkt.coord.0 += popped.size;
+                pkt.coord.0 += popped.size * self.dir;
             }
         }
         self.dropper.tick()
@@ -227,10 +261,16 @@ impl<N: Element> Element for Bottleneck<N> {
         let size = self.bufsize as f64 * DATA_PKT_WIDTH;
         let buffer = PathElement::new(
             vec![
-                (self.coord.0, self.coord.1 - PKT_HEIGHT * 0.55),
-                (self.coord.0 + size, self.coord.1 - PKT_HEIGHT * 0.55),
-                (self.coord.0 + size, self.coord.1 + PKT_HEIGHT * 0.55),
-                (self.coord.0, self.coord.1 + PKT_HEIGHT * 0.55),
+                (self.coord.0, self.coord.1 - PKT_HEIGHT * 0.55 * self.dir),
+                (
+                    self.coord.0 + size * self.dir,
+                    self.coord.1 - PKT_HEIGHT * 0.55 * self.dir,
+                ),
+                (
+                    self.coord.0 + size * self.dir,
+                    self.coord.1 + PKT_HEIGHT * 0.55 * self.dir,
+                ),
+                (self.coord.0, self.coord.1 + PKT_HEIGHT * 0.55 * self.dir),
             ],
             BLACK,
         );
@@ -244,27 +284,42 @@ impl<N: Element> Element for Bottleneck<N> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::gif("ideal.gif", (800, 600), 33)?.into_drawing_area();
+    let root = BitMapBackend::gif("ideal.gif", (800, 300), 33)?.into_drawing_area();
 
     let sink = Rc::new(RefCell::new(Sink {
-        coord: Coord(9., 0.),
+        coord: Coord(-9., -PKT_HEIGHT * 2.),
     }));
-    let departure = Rc::new(RefCell::new(Transport::new(32, sink.clone())));
+    let returnpath2 = Rc::new(RefCell::new(Transport::new(16, sink.clone())));
+    let ret_bottleneck = Rc::new(RefCell::new(Bottleneck::new(
+        Coord(DATA_PKT_WIDTH * 10., -PKT_HEIGHT * 2.),
+        10,
+        8,
+        returnpath2.clone(),
+        false,
+    )));
+    let returnpath1 = Rc::new(RefCell::new(Transport::new(16, ret_bottleneck.clone())));
+    let acker = Rc::new(RefCell::new(Acker {
+        rcv_coord: Coord(9., 0.),
+        snd_coord: Coord(9., -PKT_HEIGHT * 2.),
+        next: returnpath1.clone(),
+    }));
+    let departure = Rc::new(RefCell::new(Transport::new(16, acker.clone())));
     let bottleneck = Rc::new(RefCell::new(Bottleneck::new(
         Coord(0., 0.),
         10,
-        32,
+        16,
         departure.clone(),
+        true,
     )));
-    let arrival = Rc::new(RefCell::new(Transport::new(64, bottleneck.clone())));
+    let arrival = Rc::new(RefCell::new(Transport::new(32, bottleneck.clone())));
 
     for tick in 0..640 {
         root.fill(&WHITE)?;
-        let chart = ChartBuilder::on(&root).build_cartesian_2d(-10.0..10.0, -10.0..10.0)?;
+        let chart = ChartBuilder::on(&root).build_cartesian_2d(-10.0..10.0, -5.0..5.0)?;
 
         let mut arrival = arrival.borrow_mut();
 
-        if tick % 16 == 0 {
+        if tick % 8 == 0 {
             arrival.enqueue(&Packet {
                 size: DATA_PKT_WIDTH,
                 coord: Coord(-10., 0.),
@@ -285,6 +340,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut departure = departure.borrow_mut();
         departure.tick();
         for e in departure.draw() {
+            chart.plotting_area().draw(&e)?;
+        }
+
+        let mut acker = acker.borrow_mut();
+        acker.tick();
+        for e in acker.draw() {
+            chart.plotting_area().draw(&e)?;
+        }
+
+        let mut returnpath1 = returnpath1.borrow_mut();
+        returnpath1.tick();
+        for e in returnpath1.draw() {
+            chart.plotting_area().draw(&e)?;
+        }
+
+        let mut ret_bottleneck = ret_bottleneck.borrow_mut();
+        ret_bottleneck.tick();
+        for e in ret_bottleneck.draw() {
+            chart.plotting_area().draw(&e)?;
+        }
+
+        let mut returnpath2 = returnpath2.borrow_mut();
+        returnpath2.tick();
+        for e in returnpath2.draw() {
             chart.plotting_area().draw(&e)?;
         }
 
